162dc36738271d083ef37687785287d5a0a2a887
Karan <email@karanshergill.com>
2024-07-23 21:32:44 +0530

    configure custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Primary-Very-Dark-Blue': 'hsl(233, 47%, 7%)',
+        'Primary-Dark-Desaturated-Blue': 'hsl(244, 38%, 16%)',
+        'Primary-Soft-Voilet': 'hsl(277, 64%, 61%)',
+        'Neutral-White': 'hsl(0, 0%, 100%)',
+        'Neutral-Slightly-Transparent-White-Para': 'hsla(0, 0%, 100%, 0.75)',
+        'Neutral-Slightly-Transparent-White-Head' : 'hsla(0, 0%, 100%, 0.6)'
+      },
+      fontFamily: {
+        'Inter': ["Lexend Deca", 'sans-serif'],
+        'Lexend': ["Inter", 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
